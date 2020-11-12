df69b0a34480726e55f1090ad4da5bac03674725
mbone1 <m.bonnev10@gmail.com>
2020-11-11 17:00:05 -0700

    input to output functioning, no obfustication

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      Intentionally Obfusticated
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      obfustication: "",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    console.log(e.target)
+    this.setState({
+      obfustication: e.target.value,
+    });
+  }
+  
+
+  render() {
+    return (
+      <div>
+        Your Message :
+        <input
+          type="text"
+          value={this.state.obfustication}
+          onChange={this.handleChange}
+        />
+        <br />
+       Your Obfusticated Message : {this.state.obfustication}
+      </div>
+    );
+  }
 }
 
 export default App;

6399e15d53bf374c0f78720988a903d8a27d5f7e
mbone1 <m.bonnev10@gmail.com>
2020-11-19 12:50:01 -0700

    expanded

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, { Component } from 'react';
 import './styles/style.css'
+import selector from './test'
 
 class App extends Component {
   constructor(props) {
@@ -9,63 +10,70 @@ class App extends Component {
     this.state = {
       obfustication: "",
     };
-
     this.handleChange = this.handleChange.bind(this);
   }
 
+
   // obfusticator() {
-  //   if (e.target.value = 'a') {
-  //     return 'ă';
-  //   }
-  // }
-  obfusticator(letter) {
+    //   if (e.target.value = 'a') {
+      //     return 'ă';
+      //   }
+      // }
+      obfusticator(letter) {
+        return letter.replaceAll("a", selector())
+        // if (letter === "a") {
+        //   return "ă";
+        // }
+        //  if (letter === "b") {
+        //   return "ƀ";
+        // }
+        // else {
+        //   return letter;
+        // }
+       }
+       handleChange(e) {
+         //  function obfusticator() {
+         //    if (e.target.value = "a") {
+         //      return "ă";
+         //    }
+         //  }
+         console.log(e)
+         this.setState({
+           obfustication: e.target.value,
+         });
+       }
+       render() {
+         return (
+           <div className="panel">
+             YOUR MESSAGE:
+             <input
+               type="text"
+               value={this.state.obfustication}
+               onChange={this.handleChange}
+             />
+             <br />
+             YOUR UNOBFUSTICATED MESSAGE : {this.state.obfustication}
+             <br />
+             YOUR OBFUSTICATED MESSAGE : {this.obfusticator(this.state.obfustication)}
+           </div>
+         );
+       }
+     }
+     export default App;
+
+  
+       //take phrase entered
+       //convert 
+      
     
-   return letter.replace("a", "C")
-    // if (letter === "a") {
-    //   return "ă";
-    // }
-    //  if (letter === "b") {
-    //   return "ƀ";
-    // }
-    // else {
-    //   return letter;
-    // }
-   }
+    
+  
   
    
 
-  handleChange(e) {
-   
-    //  function obfusticator() {
-    //    if (e.target.value = "a") {
-    //      return "ă";
-    //    }
-    //  }
-    
-    console.log(e)
-    this.setState({
-      obfustication: e.target.value,
-    });
-  }
-  
-
-  render() {
-    return (
-      <div className="panel">
-        YOUR MESSAGE:
-        <input
-          type="text"
-          value={this.state.obfustication}
-          onChange={this.handleChange}
-        />
-        <br />
-        YOUR UNOBFUSTICATED MESSAGE : {this.state.obfustication}
-        <br />
-        YOUR OBFUSTICATED MESSAGE : {this.obfusticator(this.state.obfustication)}
-      </div>
-    );
-  }
-}
-export default App;
         
+   
+    
+  
+
 
---
+++ src/test2.js
@@ -0,0 +1,58 @@
+
+let phrase = "let me have some of that red stuff";
+
+export default function obfusticator() {
+    let splitter = Array.from(phrase)
+    let split = []
+    let randomizer = Math.floor(Math.random() * 10)
+    
+
+    for (let index = 0; index < splitter.length; index++) {
+        let element = splitter[index]
+        if (element = "a") {
+            let a = ["À", "Ā", "Ă", "Ą", "ą", "à", "Â", "ã", "â", "å", "ă"];
+            return split.push(a[randomizer])
+            console.log(split)
+        }
+        else if (element = "b") {
+            return console.log("spongus")
+        }
+    }
+}
+    
+    
+    
+    
+//   }
+
+
+// }
+
+
+
+// a
+// b
+// c
+// d
+// e
+// f
+// g
+// h
+// i
+// j
+// k
+// l
+// m
+// n
+// o
+// p
+// q
+// r
+// s
+// t
+// u
+// v
+// w
+// x
+// y
+// z

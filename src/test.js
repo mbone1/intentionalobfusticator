dafeec51931d03223aa52ea03e976582fbd3f937
mbone1 <m.bonnev10@gmail.com>
2020-11-28 11:15:26 -0700

    cleanup

--- src/test.js
+++
@@ -1,9 +0,0 @@
-//  export default function selector() {
-//    let a = ["À", "Ā", "Ă", "Ą", "ą", "à", "Â", "ã", "â", "å", "ă"];
-          
-//           let randomizer = Math.floor(Math.random() * 10);
-//    return a[randomizer]
-   
-// }
-        
-
